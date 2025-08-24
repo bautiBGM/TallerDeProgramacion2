3a4e03f2aae9bea1bd6085077cdc530783ea0534
BautiGayoso <Bautigayoso2001@gmail.com>
2025-08-24 15:55:30 -0300

    ejercicios resueltos

---
+++ ejercicios-clase-2.js
@@ -0,0 +1,16 @@
+//CALCULADORA DE AREA DE RECTANGULO 
+let ancho = 10;
+let alto = 30; 
+
+const resultado = ancho * alto;
+
+console.log("Resultado: ",resultado);
+
+
+// CONVERSOR DE TEMPERATURA 
+
+let gradosCelsius = 35;
+let gradosFahrenheit = (gradosCelsius * 9/5) +32;
+
+console.log("gradosFahrenheit: ",gradosFahrenheit);
+

f914d8e3b6f08091e721ba47a11a699e8e475d05
BautiGayoso <Bautigayoso2001@gmail.com>
2025-08-28 17:19:51 -0300

    ejercicio1 resuelto

--- ejercicios-clase-3.js
+++ ejercicios-clase-3.js
@@ -1,1 +1,23 @@
-//
+//Control de Flujo: Crea una función que reciba una puntuación (0-100) y devuelva una calificación usando if/else:
+// 90-100: "A"
+// 80-89: "B"
+// 70-79: "C"
+// <70: "F"
+
+//CONTROL DE FLUJO
+
+function controlFlujo(puntuacion){
+    let calificacion = ""
+    if(puntuacion >= 90 && puntuacion <= 100){
+        calificacion = "A"
+    }else if(puntuacion >= 80 && puntuacion < 90){
+        calificacion = "B"
+    }else if(puntuacion >= 70 && puntuacion < 80){
+        calificacion = "C"
+    }else{
+        calificacion = "F"
+    }
+    return console.log("Calificacion: ", calificacion);
+}
+
+controlFlujo(80)

cadd070fd1d6ab4075ccdd3eab5be9a0118ce511
BautiGayoso <Bautigayoso2001@gmail.com>
2025-08-31 19:29:48 -0300

    dos ejercicios mas resueltos

--- ejercicios-clase-3.js
+++ ejercicios-clase-3.js
@@ -1,4 +1,4 @@
-//CONTROL DE FLUJO
+// 1)Control de Flujo: Crea una función que reciba una puntuación (0-100) y devuelva una calificación usando
 
 function controlFlujo(puntuacion){
     let calificacion = ""
@@ -15,3 +15,49 @@ function controlFlujo(puntuacion){
 }
 
 controlFlujo(80)
+
+
+// 2) Operador Ternario: Crea una función esPar que use el operador ternario para devolver "Es par" o "Es impar" según el número recibido.
+
+const esPar = (n) => n % 2 === 0 ? "es par" : "es impar";
+console.log(esPar(7));
+
+
+// 3) Calculadora con switch: Crea una función calcular que reciba dos números y un operador ("+", "-", "*" o "/")
+// y devuelva el resultado usando una sentencia switch.
+
+function calculadora (n1,n2,operador){
+    let calcular;
+    switch(operador){
+        case '+':
+            calcular = n1 + n2;
+            break;
+        case '-':
+            calcular = n1 - n2;
+            break;
+        case '*':
+            calcular = n1 * n2;
+            break;
+        case '/':
+            if (n2 === 0 ){
+                calcular = "No se puede dividir por 0";
+            }else{
+                calcular = n1 / n2;
+            }
+            break;
+        default:
+            calcular = "Operador no valido";
+    }
+    return calcular
+}
+
+console.log("calcular: ",calculadora(10,5,'+'));
+
+
+// 4) Objeto "Libro": Crea un objeto libro con propiedades titulo, autor, año y un método resumen que imprima "El libro [titulo] 
+// fue escrito por [autor] en el año [año]".
+
+// 5) Contador con Closure: Crea una función crearJuego que use un closure para mantener un puntaje privado.
+// Debe devolver un objeto con dos métodos: sumarPunto() y mostrarPuntaje().
+
+// 6) Iterar Objeto: Crea una función mostrarPropiedades que reciba un objeto e imprima cada una de sus propiedades y valores en la consola.
